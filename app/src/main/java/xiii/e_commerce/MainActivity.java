efd2da6275c9a7807e1d2251d8f59ece69bdc3a3
Dmitry Gusev <dmtrxiii@gmail.com>
2021-11-15 16:53:03 +0300

    Initial commit

---
+++ app/src/main/java/xiii/e_commerce/MainActivity.java
@@ -0,0 +1,43 @@
+package xiii.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import xiii.e_commerce.adapter.CategoryAdapter;
+import xiii.e_commerce.model.Category;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Игры"));
+        categoryList.add(new Category(2, "Сайты"));
+        categoryList.add(new Category(3, "Языки"));
+        categoryList.add(new Category(4, "Прочее"));
+
+        setCategoryRecycler(categoryList);
+
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        categoryRecycler = findViewById(R.id.category_recycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
+    }
+
+}
